920c3b93304cb837123cd99605ef1a09c5056489
sspkuxuan <1801210380@pku.edu.cn>
2018-10-07 20:37:33 +0800

    Myweather

---
+++ app/src/main/java/com/example/xuan/myweather/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.xuan.myweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.myweather_info);
+    }
+}
